8ce72d649be66cf95f195d2e8f91c329913cdccc
bakunts8 <khachikbakunts@gmail.com>
2023-01-28 12:59:07 +0400

    Add first project to new repository

---
+++ src/ChristmasTree.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class ChristmasTree {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Input number of Christmas tree rows");
+        int lines = s.nextInt();
+        int x = 2 * lines + 1;
+        int y = lines;
+
+        for (int i = 0; i < lines; i++) {
+            for (int j = y; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            for (int a = 2*y+1; a <= x && a >= 0; a += 2) {
+                System.out.print("*");
+            }
+            y--;
+            x+=2;
+            System.out.println();
+        }
+    }
+}
